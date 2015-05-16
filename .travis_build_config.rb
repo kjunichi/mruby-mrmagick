6fc2e8c3ea18cb73ca788e313eef775ff26f32c3
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2015-05-16 13:00:10 +0900

    update for test code.

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -1,5 +1,7 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
-  conf.gembox 'default'
+  conf.gembox 'full-core'
+  conf.gem :git => 'https://github.com/iij/mruby-io.git'
+
   conf.gem '../mruby-mrmagick'
 end
