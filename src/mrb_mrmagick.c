f2bf149c1bde119d953ba0769ecfec2c8f8330c7
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2015-09-25 06:32:09 +0900

    Add gif animation support

--- src/mrb_mrmagick.c
+++ src/mrb_mrmagick.c
@@ -21,6 +21,7 @@ extern void scale(const char *src_path, const char *dst_path, const char*ratio);
 extern void blur(const char *src_path, const char *dst_path,
                  const double radius, const double sigma);
 extern mrb_value mrb_mrmagick_write(mrb_state *mrb, mrb_value self);
+extern mrb_value mrb_mrmagick_write_gif(mrb_state *mrb, mrb_value self);
 extern mrb_value mrb_mrmagick_get_exif_by_entry(mrb_state *mrb, mrb_value self);
 extern mrb_value mrb_mrmagick_to_blob(mrb_state *mrb, mrb_value self);
 extern mrb_value mrb_mrmagick_from_blob(mrb_state *mrb, mrb_value self);
@@ -110,6 +111,7 @@ void mrb_mruby_mrmagick_gem_init(mrb_state *mrb)
   mrb_define_class_method(mrb, mrmagick, "blur", mrb_mrmagick_blur, MRB_ARGS_REQ(4));
   mrb_define_class_method(mrb, mrmagick, "rm", mrb_mrmagick_rm, MRB_ARGS_REQ(1));
   mrb_define_class_method(mrb, mrmagick, "write", mrb_mrmagick_write, MRB_ARGS_REQ(1));
+  mrb_define_class_method(mrb, mrmagick, "write_gif", mrb_mrmagick_write_gif, MRB_ARGS_REQ(1));
   mrb_define_class_method(mrb, mrmagick, "to_blob", mrb_mrmagick_to_blob, MRB_ARGS_REQ(1));
   mrb_define_class_method(mrb, mrmagick, "from_blob", mrb_mrmagick_to_blob, MRB_ARGS_REQ(2));
   mrb_define_class_method(mrb, mrmagick, "get_exif_by_entry", mrb_mrmagick_get_exif_by_entry, MRB_ARGS_REQ(1));
