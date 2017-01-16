b4bfb3cb4ec587cfdb42a83ffab9412501045ca3
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-01-16 16:02:45 +0900

    Update .travis_build_config.rb

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -1,7 +1,7 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
   conf.gembox 'full-core'
-
+  conf.disable_cxx_exception
   conf.gem '../mruby-mrmagick'
 end
 
@@ -9,11 +9,12 @@ MRuby::Build.new('test') do |conf|
   toolchain :gcc
 
   enable_debug
+  conf.disable_cxx_exception
   conf.enable_bintest
   conf.enable_test
 
   conf.gembox 'full-core'
   
-  conf.gem :git => 'https://github.com/iij/mruby-io.git'
+  conf.gem :github => 'iij/mruby-io.git'
   conf.gem '../mruby-mrmagick'
 end
